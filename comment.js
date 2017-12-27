6695d2e5392e0f0aa81b24dd98490522e6ac4b7b
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-27 02:20:22 -0800

    add comment form

--- comment.js
+++ comment.js
@@ -108,3 +108,44 @@ class Comment extends React.Component {
     );
   }
 }
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
+        <label>New comment</label>
+        <div className="comment-form-fields">
+          <input placeholder="Name:" ref={input => this._author = input} />
+          <textarea
+            placeholder="Comment:"
+            ref={textarea => this._body = textarea} >
+            onKeyUp={this._getCharacterCount.bind(this)}
+          </textarea>
+        </div>
+        <div className="comment-form-actions">
+          <button type="submit">
+            Post comment
+          </button>
+        </div>
+      </form>
+    );
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.addComment(this._author.value, this._body.value);
+
+    this._author.value = '';
+    this._body.value = '';
+
+    this.setState({ characters: 0 });
+  }
+
+  _getCharacterCount(){
+    this.setState({
+      characters: this._body.value.length
+    });
+  }
+
+}
